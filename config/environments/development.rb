8731b3871f713f2eea6f02140fa91372791c2b00
Long <longvo@Longs-MacBook-Pro.local>
2014-12-16 15:34:57 +0700

    add configuration to send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,22 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com'
+    port: 587,
+    domain: Rails.application.serects.domain_name
+    authenticate: 'plain'
+    enable_starttls_auto: true
+    user_name: Rails.application.serects.email_provider_username
+    password: Rails.application.serects.email_provider_password
+  }
+
+  #ActionMailer Config
+  config.action.mailer.default_url_options = {
+    :host => "localhost:3000"
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
